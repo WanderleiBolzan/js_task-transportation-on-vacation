cd210f2188e53b8ec62353086d36324e3dc7c843
wanderlei jose bolzan <wbolzanjp@gmail.com>
2025-05-14 11:46:47 -0300

    commit lesson

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,24 @@
  *
  * @return {number}
  */
+const rentalDay = 40;
+
 function calculateRentalCost(days) {
   // write code here
-}
+  if (days === 0) {
+    return 0;
+  }
 
+  if (days < 3) {
+    return rentalDay;
+  }
+
+  if (days === 3) {
+    return rentalDay * days - 20;
+  }
+
+  if (days > 3 && days <= 7) {
+    return rentalDay * days - 50;
+  }
+}
 module.exports = calculateRentalCost;
